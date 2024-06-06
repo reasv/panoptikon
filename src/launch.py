1fa54453df6757d41ab821d966cc4f0a2e43f7f0
reasv <7143787+reasv@users.noreply.github.com>
2024-06-06 18:41:18 +0200

    Fix connection

--- src/launch.py
+++ src/launch.py
@@ -3,7 +3,7 @@ from src.db import initialize_database, get_database_connection
 
 def launch_app():
     conn = get_database_connection()
-    initialize_database()
+    initialize_database(conn)
     conn.commit()
     conn.close()
     create_root_UI()
