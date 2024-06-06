9e4be2d94f2014945a3ab598f8ac2eb97041112e
reasv <7143787+reasv@users.noreply.github.com>
2024-06-06 04:59:08 +0200

    Remove unused

--- scan.py
+++
@@ -1,8 +0,0 @@
-
-from src import load_paths_from_file, save_items_to_database, scan_images
-
-if __name__ == '__main__':
-    file_path = 'paths.txt'
-    starting_points = load_paths_from_file(file_path)
-    hashes_info = scan_images(starting_points)
-    save_items_to_database(hashes_info, starting_points)
--- scan_tags.py
+++
@@ -1,5 +0,0 @@
-
-from src import scan_and_predict_tags
-
-if __name__ == '__main__':
-    scan_and_predict_tags()
