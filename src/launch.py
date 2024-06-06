3a83b4a61dece9dfb941a2f5fec15f40f11b555c
reasv <7143787+reasv@users.noreply.github.com>
2024-06-06 19:20:16 +0200

    Readonly mode

--- src/launch.py
+++ src/launch.py
@@ -1,4 +1,14 @@
-from src.ui.root import create_root_UI
+import os
+
+readonly_mode = os.environ.get('READONLY', 'false').lower() == 'true'
+
+if readonly_mode:
+    import src.ui.root_readonly as root
+    print("Running in read-only mode")
+else:
+    import src.ui.root as root
+    print("Running in read-write mode")
+
 from src.db import initialize_database, get_database_connection
 
 def launch_app():
@@ -6,4 +16,4 @@ def launch_app():
     initialize_database(conn)
     conn.commit()
     conn.close()
-    create_root_UI()
+    root.create_root_UI()
---
+++ src/ui/root_readonly.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import gradio as gr
+
+from src.ui.toptags import create_toptags_UI
+from src.ui.search import create_search_UI
+
+def create_root_UI():
+    with gr.Blocks(css="static/style.css", fill_height=True) as ui:
+        with gr.Tabs():
+            with gr.TabItem(label="Tag Search"):
+                create_search_UI()
+            with gr.TabItem(label="Tag Frequency"):
+                create_toptags_UI()
+    ui.launch()
