43d96bada4e3c81892bc9cb4ba5e63c8da5242e3
reasv <7143787+reasv@users.noreply.github.com>
2024-06-08 23:29:51 +0200

    Db bookmarks functions

--- src/db.py
+++ src/db.py
@@ -6,8 +6,13 @@ from typing import Dict, List, Tuple
 from src.utils import normalize_path
 
 def get_database_connection() -> sqlite3.Connection:
+    # Check if we are in read-only mode
     db_file = os.getenv('DB_FILE', 'sqlite.db')
-    conn = sqlite3.connect(db_file)
+    if os.environ.get('READONLY', 'false').lower() == 'true':
+        # Use a read-only connection
+        conn = sqlite3.connect(f'file:{db_file}?mode=ro', uri=True)
+    else:
+        conn = sqlite3.connect(db_file)
     return conn
 
 def initialize_database(conn: sqlite3.Connection):
@@ -566,7 +571,7 @@ def update_bookmarks(conn: sqlite3.Connection, items_sha256: List[str], namespac
     cursor.executemany('''
     INSERT INTO bookmarks (namespace, sha256, time_added)
     VALUES (?, ?, ?)
-    ON CONFLICT(sha256) DO NOTHING
+    ON CONFLICT(namespace, sha256) DO NOTHING
     ''', [(namespace, sha256, datetime.now().isoformat()) for sha256 in items_sha256])
 
     # Remove all items that are not in the list
@@ -575,9 +580,84 @@ def update_bookmarks(conn: sqlite3.Connection, items_sha256: List[str], namespac
     WHERE sha256 NOT IN ({}) AND namespace = ?
     '''.format(','.join(['?']*len(items_sha256)), items_sha256, namespace))
 
-def get_bookmarks(conn: sqlite3.Connection, namespace: str = 'default') -> List[Tuple[str, str]]:
+def add_bookmark(conn: sqlite3.Connection, sha256: str, namespace: str='default', metadata: str=None):
     cursor = conn.cursor()
+    cursor.execute('''
+    INSERT INTO bookmarks (namespace, sha256, time_added, metadata)
+    VALUES (?, ?, ?, ?)
+    ON CONFLICT(namespace, sha256) DO NOTHING
+    ''', (namespace, sha256, datetime.now().isoformat(), metadata))
+
+def remove_bookmark(conn: sqlite3.Connection, sha256: str, namespace: str='default'):
+    cursor = conn.cursor()
+    cursor.execute('''
+    DELETE FROM bookmarks
+    WHERE sha256 = ? AND namespace = ?
+    ''', (sha256, namespace))
+
+def toggle_bookmark_db(conn: sqlite3.Connection, sha256: str, namespace: str='default', metadata: str=None):
+    cursor = conn.cursor()
+    # Use ON CONFLICT to toggle the bookmark
+    cursor.execute('''
+    INSERT INTO bookmarks (namespace, sha256, time_added, metadata)
+    VALUES (?, ?, ?, ?)
+    ON CONFLICT(sha256) DO DELETE
+    ''', (namespace, sha256, datetime.now().isoformat(), metadata))
+
+def get_bookmark_metadata(conn: sqlite3.Connection, sha256: str, namespace: str='default'):
+    cursor = conn.cursor()
+    cursor.execute('''
+    SELECT metadata
+    FROM bookmarks
+    WHERE sha256 = ? AND namespace = ?
+    ''', (sha256, namespace))
+    metadata = cursor.fetchone()
+    
+    return True, metadata[0] if metadata else False, None
+
+def delete_bookmarks_exclude_last_n(conn: sqlite3.Connection, n: int, namespace: str = 'default'):
+    cursor = conn.cursor()
+    # Delete all bookmarks except the last n based on time_added
+    cursor.execute('''
+        DELETE FROM bookmarks
+        WHERE namespace = ?
+        AND sha256 NOT IN (
+            SELECT sha256
+            FROM bookmarks
+            WHERE namespace = ?
+            ORDER BY time_added DESC
+            LIMIT ?
+        )
+    ''', (namespace, namespace, n))
+    
+    conn.commit()
+
+def get_all_bookmark_namespaces(conn: sqlite3.Connection) -> List[str]:
+    cursor = conn.cursor()
+    # Get all bookmark namespaces, order by namespace name
+    cursor.execute('''
+        SELECT DISTINCT namespace
+        FROM bookmarks
+        ORDER BY namespace
+    ''')
+    namespaces = cursor.fetchall()
+    return [namespace[0] for namespace in namespaces]
+
+def get_bookmarks(conn: sqlite3.Connection, namespace: str = 'default', page_size=1000, page=1,) -> List[Tuple[str, str]]:
+    if page_size < 1:
+        page_size = 1000000
+    offset = (page - 1) * page_size
+
     # Fetch bookmarks with their paths, prioritizing available files
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT COUNT(DISTINCT bookmarks.sha256)
+        FROM bookmarks
+        LEFT JOIN files ON bookmarks.sha256 = files.sha256
+        WHERE bookmarks.namespace = ?
+    ''', (namespace,))
+    total_results = cursor.fetchone()[0]
+
     cursor.execute('''
         SELECT bookmarks.sha256, 
                COALESCE(available_files.path, any_files.path) as path
@@ -589,8 +669,9 @@ def get_bookmarks(conn: sqlite3.Connection, namespace: str = 'default') -> List[
                ON bookmarks.sha256 = any_files.sha256
         WHERE bookmarks.namespace = ?
         GROUP BY bookmarks.sha256
-        ORDER BY bookmarks.time_added ASC
-    ''', (namespace,))
+        ORDER BY bookmarks.time_added DESC
+        LIMIT ? OFFSET ?
+    ''', (namespace, page_size, offset))
     
     bookmarks = cursor.fetchall()
     bookmark_tuples: List[Tuple[str, str]] = [(bookmark[0], bookmark[1]) for bookmark in bookmarks]
@@ -601,4 +682,4 @@ def get_bookmarks(conn: sqlite3.Connection, namespace: str = 'default') -> List[
             if result := get_working_path_by_sha256(conn, bookmark[0]):
                 bookmark_tuples[i] = (bookmark[0], result[0])
     
-    return bookmark_tuples
+    return bookmark_tuples, total_results
