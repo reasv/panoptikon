53404e12f2a00bb6c80a8e59a9f487c97f42cc0a
reasv <7143787+reasv@users.noreply.github.com>
2024-06-08 08:17:42 +0200

    Fix history page

--- src/ui/history.py
+++ src/ui/history.py
@@ -1,21 +1,17 @@
 from __future__ import annotations
 from typing import List
+import json
 
 import gradio as gr
-from src.db import get_all_tags_for_item_name_confidence, get_database_connection
+from src.ui.components.list_view import create_image_list
+from src.ui.components.gallery_view import create_gallery_view
 
-def get_history_paths(select_history: gr.State):
+def get_history_paths(select_history: List[str]):
     print(f"History length is {len(select_history)}")
     # Should be in reverse order
-    reverse = [item['path'] for item in select_history[::-1]]
-    return reverse
-
-def get_history_list(select_history: List[dict]):
-    reverse = [[item['path'], item['path'], item["sha256"]] for item in select_history[::-1]]
-    return gr.update(samples=reverse)
-
-def set_columns(columns: int):
-    return gr.update(columns=columns)
+    reverse = [(item['path'], json.dumps(item)) for item in select_history[::-1]]
+    reverse_list = [[item['path'], item['path'], item["sha256"]] for item in select_history[::-1]]
+    return reverse, gr.update(samples=reverse_list)
 
 def erase_history_fn(select_history: List[str], keep_last_n: int):
     if keep_last_n > 0:
@@ -23,16 +19,11 @@ def erase_history_fn(select_history: List[str], keep_last_n: int):
     else:
         select_history = []
     print("History erased")
-    return select_history, gr.update(value=select_history), gr.update(samples=[])
+    gallery_update, list_update = get_history_paths(select_history)
+    return select_history, gallery_update, list_update
 
-def update_selected(dataset_data):
-    sha256 = dataset_data[2]
-    conn = get_database_connection()
-    tags = { t[0]: t[1] for t in get_all_tags_for_item_name_confidence(conn, sha256)}
-    conn.close()
-    # Tags in the format "tag1, tag2, tag3"
-    text = ", ".join(tags.keys())
-    return dataset_data[1], tags, text
+def on_gallery_select_image(selected_image_path: str, selected_image_sha256: str):
+    return selected_image_path, selected_image_sha256
 
 def create_history_UI(select_history: gr.State):
     with gr.TabItem(label="History") as history_tab:
@@ -42,46 +33,25 @@ def create_history_UI(select_history: gr.State):
                 keep_last_n = gr.Slider(minimum=0, maximum=100, value=0, step=1, label="Keep last N items on erase")
         with gr.Tabs():
             with gr.TabItem(label="Gallery"):
-                columns_slider = gr.Slider(minimum=1, maximum=10, value=5, step=1, label="Number of columns")
-                image_output = gr.Gallery(label="Results", scale=2, columns=5)
+                history_gallery = create_gallery_view()
 
-            with gr.TabItem(label="List") as list_tab:
-                with gr.Row():
-                    history_list = gr.Dataset(label="History", type="values", samples_per_page=25, samples=[], components=["image", "textbox"], scale=1)
-
-                    selected_image = gr.Image(label="Selected Image", elem_id="bighistoryPreview", interactive=False, scale=2)
-                    with gr.Tabs():
-                        with gr.TabItem(label="Tags"):
-                            tag_labels = gr.Label(label="Image Tags", scale=2)
-                        with gr.TabItem(label="Tags List"):
-                            tag_text = gr.Textbox(label="Tags", interactive=False, lines=5, scale=2)
-
-    history_list.click(
-        fn=update_selected,
-        inputs=history_list,
-        outputs=[selected_image, tag_labels, tag_text]
-    )
+            with gr.TabItem(label="List"):
+                history_list = create_image_list()
 
     history_tab.select(
         fn=get_history_paths,
         inputs=[select_history],
-        outputs=[image_output]
-    )
-
-    list_tab.select(
-        fn=get_history_list,
-        inputs=[select_history],
-        outputs=[history_list]
-    )
-
-    columns_slider.release(
-        fn=set_columns,
-        inputs=[columns_slider],
-        outputs=[image_output]
+        outputs=[history_gallery.image_output, history_list.file_list]
     )
 
     erase_history.click(
         fn=erase_history_fn,
         inputs=[select_history, keep_last_n],
-        outputs=[select_history, image_output, history_list]
+        outputs=[select_history, history_gallery.image_output, history_list.file_list]
+    )
+
+    history_gallery.selected_image_path.change(
+        fn=on_gallery_select_image,
+        inputs=[history_gallery.selected_image_path, history_gallery.selected_image_sha256],
+        outputs=[history_list.selected_image_path, history_list.selected_image_sha256]
     )
