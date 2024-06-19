21117829833e88d89eec52fc3a7e0588941caa8e
reasv <7143787+reasv@users.noreply.github.com>
2024-06-19 15:56:24 +0200

    Remove threads from tag scanning

--- src/tags.py
+++ src/tags.py
@@ -33,6 +33,9 @@ def process_video_dd(sha256: str, video_path: str, model, labels, keyframe_thres
     except Exception as e:
         print(f"Error processing video {video_path}: {e}")
         return None, None
+    if not frames:
+        print(f"No frames found for {video_path}")
+        return None, None
     results = []
     for result_threshold, _result_all, _result_text in predict_batch(frames, model, labels, score_threshold=tag_threshold):
         results.append((result_threshold))
@@ -56,6 +59,9 @@ def process_single_file(sha256: str, mime_type: str, path: str, model, labels, t
         return None, 0
 
 def scan_and_predict_tags(conn: sqlite3.Connection, setter="deepdanbooru"):
+    """
+    Scan and predict tags for all items in the database that are missing tags from the given tagging ML model.
+    """
     scan_time = datetime.now().isoformat()
     model = load_model()
     labels = load_labels()
@@ -63,55 +69,45 @@ def scan_and_predict_tags(conn: sqlite3.Connection, setter="deepdanbooru"):
     print(f"Using score threshold {score_threshold}")
     timeout = get_timeout_from_env()
     failed_paths = []
-    timeouts = []
     videos, images, total_video_frames, total_processed_frames = 0, 0, 0, 0
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for item in get_items_missing_tags(conn, setter):
-            print(f"Processing {item.path} ({item.type}) (timeout {timeout})")
-            future = executor.submit(
-                    process_single_file,
-                    item.sha256,
-                    item.type,
-                    item.path,
-                    model,
-                    labels,
-                    tag_threshold=score_threshold
-                )
-            try:
-                result_threshold, frames = future.result(timeout=timeout)
-            except concurrent.futures.TimeoutError:
-                print(f"Timeout processing {item.path}")
-                timeouts.append(item.path)
-                continue
-            total_processed_frames += frames
+    for item in get_items_missing_tags(conn, setter):
+        print(f"Processing {item.path} ({item.type})")
+        result_threshold, frames = process_single_file(
+                item.sha256,
+                item.type,
+                item.path,
+                model,
+                labels,
+                tag_threshold=score_threshold
+            )
+        if result_threshold is None:
+            failed_paths.append(item.path)
+            continue
 
-            if item.type.startswith("video"):
-                videos += 1
-                total_video_frames += frames
-            else:
-                images += 1
+        total_processed_frames += frames
 
-            if result_threshold is None:
-                failed_paths.append(item.path)
-                continue
-            print(f"Adding {len(result_threshold.keys())} tags for {item.path}...")
-            for tag, confidence in result_threshold.items():
-                insert_tag(
-                    conn,
-                    scan_time=scan_time,
-                    namespace="danbooru",
-                    name=tag,
-                    item=item.sha256,
-                    confidence=confidence,
-                    setter=setter,
-                    value=None
-                )
-            print(f"Added tags for {item.path}")
+        if item.type.startswith("video"):
+            videos += 1
+            total_video_frames += frames
+        else:
+            images += 1
 
+        for tag, confidence in result_threshold.items():
+            insert_tag(
+                conn,
+                scan_time=scan_time,
+                namespace="danbooru",
+                name=tag,
+                item=item.sha256,
+                confidence=confidence,
+                setter=setter,
+                value=None
+            )
     print(f"Processed {images} images and {videos} videos totalling {total_processed_frames} frames ({total_video_frames} video frames)")
+    
+    # Record the scan in the database log
     scan_end_time = datetime.now().isoformat()
     remaining_paths = len(list(get_items_missing_tags(conn, setter)))
-
     add_tag_scan(
         conn,
         scan_time,
@@ -124,8 +120,9 @@ def scan_and_predict_tags(conn: sqlite3.Connection, setter="deepdanbooru"):
         video_frames=total_video_frames,
         total_frames=total_processed_frames,
         errors=len(failed_paths),
-        timeouts=len(timeouts),
-        total_remaining=len(remaining_paths)
+        timeouts=0,
+        total_remaining=remaining_paths
     )
+    print("Added scan to database")
 
-    return images, videos, failed_paths, timeouts
+    return images, videos, failed_paths, []
