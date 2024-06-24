a5d95648c395e1228d5421d1a69ec38ce4811f80
reasv <7143787+reasv@users.noreply.github.com>
2024-06-24 16:54:09 +0200

    Remove dead dd code

--- src/ui/test_model.py
+++ src/ui/test_model.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import gradio as gr
 import PIL.Image
 
-# from src.deepdanbooru import predict, load_labels, load_model
 from src.wd_tagger import Predictor, V3_MODELS
 from src.ui.paddleocr import create_paddleocr_UI
 from src.ui.clip import create_CLIP_ui
@@ -130,38 +129,3 @@ def create_wd_tagger_UI():
         ],
         outputs=[rating, character_res, general_res, sorted_general_strings],
     )
-
-# def create_dd_UI():
-#     DESCRIPTION = "# [KichangKim/DeepDanbooru](https://github.com/KichangKim/DeepDanbooru)"
-#     model = None
-#     labels = None
-#     def predict_load(image: PIL.Image.Image, score_threshold: float) -> tuple[dict[str, float], dict[str, float], str]:
-#         nonlocal model, labels
-#         if model is None:
-#             model = load_model()
-#         if labels is None:
-#             labels = load_labels()
-#         return predict(image, model, labels, score_threshold)
-
-#     with gr.Column():
-#         gr.Markdown(DESCRIPTION)
-#         gr.Markdown("## Test Tagging Model")
-#         with gr.Row():
-#             with gr.Column():
-#                 image = gr.Image(label="Input", type="pil")
-#                 score_threshold = gr.Slider(label="Score threshold", minimum=0, maximum=1, step=0.05, value=0.3)
-#                 run_button = gr.Button("Run")
-#             with gr.Column():
-#                 with gr.Tabs():
-#                     with gr.Tab(label="Output"):
-#                         result = gr.Label(label="Output", show_label=False)
-#                     with gr.Tab(label="JSON"):
-#                         result_json = gr.JSON(label="JSON output", show_label=False)
-#                     with gr.Tab(label="Text"):
-#                         result_text = gr.Text(label="Text output", show_label=False, lines=5)
-#     run_button.click(
-#         fn=predict_load,
-#         inputs=[image, score_threshold],
-#         outputs=[result, result_json, result_text],
-#         api_name="predict",
-#     )
