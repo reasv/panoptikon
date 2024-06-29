b2f4498d8f5ad032c0375b245ba5c4a69e61bee8
reasv <7143787+reasv@users.noreply.github.com>
2024-06-29 19:31:52 +0200

    Improve UI layout for semantic search

--- src/ui/semantic_search.py
+++ src/ui/semantic_search.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import gradio as gr
+import numpy as np
 
 from src.db import get_database_connection
 from src.ui.components.multi_view import create_multiview
@@ -13,44 +14,74 @@ def create_semantic_search_UI(
     with gr.TabItem(label="Semantic Search") as search_tab:
         with gr.Column(elem_classes="centered-content", scale=0):
             with gr.Row():
-                search_text = gr.Textbox(
-                    label="Search for images",
-                    placeholder="Search for images",
-                    lines=1,
-                    scale=3
-                )
-                n_results = gr.Slider(
-                    label="Number of results",
-                    value=10,
-                    minimum=1,
-                    maximum=500,
-                    step=1,
-                    scale=1
-                )
-                submit_button = gr.Button("Search", scale=0)
+                with gr.Column():
+                    with gr.Tabs():
+                        with gr.Tab(label="Search by Text"):
+                            with gr.Row():
+                                search_text = gr.Textbox(
+                                    label="Search for images",
+                                    placeholder="Search for images",
+                                    lines=1,
+                                    scale=3
+                                )
+                                submit_button = gr.Button("Search", scale=0)
+                        with gr.Tab(label="Search by Image"):
+                            search_image = gr.Image(label="Search for similar images", scale=1, type="numpy")
+                            submit_button_image = gr.Button("Search", scale=0)
+                with gr.Column():
+                    n_results = gr.Slider(
+                            label="Number of results",
+                            value=10,
+                            minimum=1,
+                            maximum=500,
+                            step=1,
+                            scale=1
+                    )
+                    unload_model = gr.Button("Unload Model", scale=0, interactive=False)
     
         multiview = create_multiview(
             select_history=select_history,
             bookmarks_namespace=bookmarks_namespace
         )
+
     embedder = CLIPEmbedder(model_name="ViT-H-14-378-quickgelu", pretrained="dfn5b")
 
-    def items_semantic_search(search_text: str, n_results: int):
+    def items_semantic_search(search_text: str | None, search_image: np.ndarray | None, n_results: int):
         conn = get_database_connection()
         cdb = get_chromadb_client()
         files, scores = search_item_image_embeddings(
             conn,
             cdb,
             embedder,
-            image_query=None,
+            image_query=search_image,
             text_query=search_text,
             limit=n_results,
         )
-        return files
+        return files, gr.update(interactive=True)
 
+    def search_by_image(search_image: np.ndarray, n_results: int):
+        return items_semantic_search(None, search_image, n_results)
+    
+    def search_by_text(search_text: str, n_results: int):
+        return items_semantic_search(search_text, None, n_results)
+
+    def on_unload_model():
+        embedder.unload_model()
+        return gr.update(interactive=False)
+    
     submit_button.click(
-        fn=items_semantic_search,
+        fn=search_by_text,
         inputs=[search_text, n_results],
-        outputs=[multiview.files]
+        outputs=[multiview.files, unload_model]
     )
 
+    submit_button_image.click(
+        fn=search_by_image,
+        inputs=[search_image, n_results],
+        outputs=[multiview.files, unload_model]
+    )
+
+    unload_model.click(
+        fn=on_unload_model,
+        outputs=[unload_model]
+    )
