8269b74a1dfaf8db7800e22fb4df121e087ff7d9
reasv <7143787+reasv@users.noreply.github.com>
2024-07-20 15:46:02 +0200

    Add whisperx test page

--- src/ui/test_models/__init__.py
+++ src/ui/test_models/__init__.py
@@ -5,6 +5,7 @@ import gradio as gr
 from src.ui.test_models.clip import create_CLIP_ui
 from src.ui.test_models.ocr_doctr import create_doctr_UI
 from src.ui.test_models.tagging import create_wd_tagger_UI
+from src.ui.test_models.whisper import create_whisper_ui
 
 
 def create_model_demo():
@@ -16,3 +17,5 @@ def create_model_demo():
                 create_doctr_UI()
             with gr.Tab(label="CLIP Semantic Search"):
                 create_CLIP_ui()
+            with gr.Tab(label="Whisper STT"):
+                create_whisper_ui()
