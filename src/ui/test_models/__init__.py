acc56af55949581981560ec7cebab81c66c887e0
reasv <7143787+reasv@users.noreply.github.com>
2024-07-24 15:07:06 +0200

    Florence 2 model demo

--- src/ui/test_models/__init__.py
+++ src/ui/test_models/__init__.py
@@ -4,6 +4,7 @@ import gradio as gr
 
 from src.ui.test_models.clip import create_CLIP_ui
 from src.ui.test_models.fasterw import create_whisper_ui
+from src.ui.test_models.florence2 import create_florence_2_ui
 from src.ui.test_models.ocr_doctr import create_doctr_UI
 from src.ui.test_models.tagging import create_wd_tagger_UI
 
@@ -19,3 +20,5 @@ def create_model_demo():
                 create_CLIP_ui()
             with gr.Tab(label="Whisper STT"):
                 create_whisper_ui()
+            with gr.Tab(label="Florence 2"):
+                create_florence_2_ui()
