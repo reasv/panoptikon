b5de70a20f9753ad14e530b8ef7e1ace43c681e6
reasv <7143787+reasv@users.noreply.github.com>
2024-08-07 14:49:35 +0200

    Re-add single audio function

--- src/data_extractors/data_loaders/audio.py
+++ src/data_extractors/data_loaders/audio.py
@@ -241,3 +241,50 @@ def load_audio(file: str, sr: int = SAMPLE_RATE) -> List[np.ndarray]:
         raise RuntimeError(f"FFmpeg failed: {e.stderr.decode()}") from e
     except Exception as e:
         raise RuntimeError(f"An unexpected error occurred: {str(e)}") from e
+
+
+def load_audio_single(file: str, sr: int = SAMPLE_RATE) -> List[np.ndarray]:
+    """
+    Open an audio file and read as mono waveform, resampling as necessary
+
+    Parameters
+    ----------
+    file: str
+        The audio file to open
+
+    sr: int
+        The sample rate to resample the audio if necessary
+
+    Returns
+    -------
+    A NumPy array containing the audio waveform, in float32 dtype.
+    """
+    try:
+        # Launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI to be installed.
+        cmd = [
+            "ffmpeg",
+            "-nostdin",
+            "-threads",
+            "0",
+            "-i",
+            file,
+            "-f",
+            "s16le",
+            "-ac",
+            "1",
+            "-acodec",
+            "pcm_s16le",
+            "-ar",
+            str(sr),
+            "-",
+        ]
+        out = subprocess.run(cmd, capture_output=True, check=True).stdout
+    except subprocess.CalledProcessError as e:
+        if not check_audio_stream(file):
+            return []
+        raise RuntimeError(
+            f"Failed to load audio: {format_ffmpeg_error(e.stderr.decode())}"
+        ) from e
+
+    return [np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0]
