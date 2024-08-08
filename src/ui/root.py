2aeec0563297f8d42db873ca58af79dd66b6ac8f
reasv <7143787+reasv@users.noreply.github.com>
2024-08-09 00:26:26 +0200

    Disable gradio telemetry

--- src/ui/root.py
+++ src/ui/root.py
@@ -13,7 +13,9 @@ from src.ui.toptags import create_toptags_UI
 
 
 def create_root_UI():
-    with gr.Blocks(css="static/style.css", fill_height=True) as ui:
+    with gr.Blocks(
+        css="static/style.css", fill_height=True, analytics_enabled=False
+    ) as ui:
         select_history = gr.State(value=[])
         bookmarks_namespace = gr.State(value="default")
         with gr.Tabs():
