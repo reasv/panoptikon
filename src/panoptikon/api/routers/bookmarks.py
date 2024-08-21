e3569715f95c940441a5e7f183b94b24928a1e4e
reasv <7143787+reasv@users.noreply.github.com>
2024-08-21 15:13:00 +0200

    Better bookmarks API docs

--- src/panoptikon/api/routers/bookmarks.py
+++ src/panoptikon/api/routers/bookmarks.py
@@ -1,5 +1,7 @@
 import logging
 import sqlite3
+from dataclasses import dataclass
+from re import M
 from typing import Dict, List, Literal, Optional
 
 from fastapi import APIRouter, Body, Depends, Query
@@ -15,6 +17,7 @@ from panoptikon.db.bookmarks import (
     remove_bookmark,
 )
 from panoptikon.db.search.types import OrderType
+from panoptikon.types import FileSearchResult
 
 logger = logging.getLogger(__name__)
 
@@ -25,31 +28,47 @@ router = APIRouter(
 )
 
 
-@router.get("/", summary="Get all bookmark namespaces")
+@dataclass
+class BookmarkNamespaces:
+    namespaces: List[str]
+
+
+@router.get(
+    "/",
+    summary="Get all bookmark namespaces",
+    response_model=BookmarkNamespaces,
+)
 def get_ns_list(
     conn: sqlite3.Connection = Depends(get_db_readonly),
 ):
-    return {"namespaces": get_all_bookmark_namespaces(conn)}
+    return BookmarkNamespaces(namespaces=get_all_bookmark_namespaces(conn))
 
 
 class Items(BaseModel):
     sha256: List[str]
 
 
+@dataclass
+class Results:
+    count: int
+    results: List[FileSearchResult]
+
+
 @router.get(
     "/{namespace}",
     summary="Get all bookmarks in a namespace",
     description="""
-    Get all items bookmarked in namespace.
-    Note that unlike the search API, this returns unique items, not files.
-    This has two implications:
-    1. Results are unique by `sha256` value.
-    2. Even if multiple files have the same `sha256` value, they will only appear once in the results, with the path of the first reachable file found.
+Get all items bookmarked in namespace.
+Note that unlike the search API, this returns unique items, not files.
+This has two implications:
+1. Results are unique by `sha256` value.
+2. Even if multiple files have the same `sha256` value, they will only appear once in the results, with the path of the first reachable file found.
 
-    The `order_by` parameter can be used to sort the results by `last_modified`, `path`, or `time_added`.
-    The `order` parameter can be used to sort the results in ascending or descending order.
-    The `include_wildcard` parameter can be used to include bookmarks with the `*` user value.
+The `order_by` parameter can be used to sort the results by `last_modified`, `path`, or `time_added`.
+The `order` parameter can be used to sort the results in ascending or descending order.
+The `include_wildcard` parameter can be used to include bookmarks with the `*` user value.
     """,
+    response_model=Results,
 )
 def get_bookmarks_by_namespace(
     namespace: str,
@@ -63,28 +82,32 @@ def get_bookmarks_by_namespace(
     include_wildcard: bool = Query(True),
     conn: sqlite3.Connection = Depends(get_db_readonly),
 ):
-    files, count = list(
-        get_bookmarks(
-            conn,
-            namespace=namespace,
-            user=user,
-            page_size=page_size,
-            page=page,
-            order_by=order_by,
-            order=order,
-            include_wildcard=include_wildcard,
-        )
+    files, count = get_bookmarks(
+        conn,
+        namespace=namespace,
+        user=user,
+        page_size=page_size,
+        page=page,
+        order_by=order_by,
+        order=order,
+        include_wildcard=include_wildcard,
     )
-    return {"count": count, "results": files}
+    return Results(count=count, results=files)
+
+
+@dataclass
+class MessageResult:
+    message: str
 
 
 @router.delete(
     "/{namespace}",
     summary="Delete multiple bookmarks in a namespace",
     description="""
-    Delete all bookmarks in a namespace. If `exclude_last_n` is provided, the last `n` added bookmarks will be kept.
-    Alternatively, a list of `sha256` values can be provided in the request body to only delete specific bookmarks.
-    """,
+Delete all bookmarks in a namespace. If `exclude_last_n` is provided, the last `n` added bookmarks will be kept.
+Alternatively, a list of `sha256` values can be provided in the request body to only delete specific bookmarks.
+""",
+    response_model=MessageResult,
 )
 def delete_bookmarks_by_namespace(
     namespace: str,
@@ -94,9 +117,11 @@ def delete_bookmarks_by_namespace(
     conn: sqlite3.Connection = Depends(get_db_user_data_wl),
 ):
     if items:
+        c = 0
         for s in items.sha256:
             remove_bookmark(conn, s, namespace=namespace, user=user)
-        return {"message": "Deleted bookmarks"}
+            c += 1
+        return MessageResult(message=f"Deleted {c} bookmarks")
     elif items is None:
         delete_bookmarks_exclude_last_n(
             conn,
@@ -104,9 +129,9 @@ def delete_bookmarks_by_namespace(
             namespace=namespace,
             user=user,
         )
-        return {"message": "Deleted bookmarks"}
+        return MessageResult(message="Deleted bookmarks")
     else:
-        return {"message": "No items to delete"}
+        return MessageResult(message="No items provided")
 
 
 class ItemsMeta(BaseModel):
@@ -118,32 +143,33 @@ class ItemsMeta(BaseModel):
     "/{namespace}",
     summary="Add multiple bookmarks to a namespace",
     description="""
-        Add multiple bookmarks to a namespace.
-        The `sha256` values of the items to be bookmarked should be provided in the request body.
-        Optionally, metadata can be provided.
-        If metadata is provided, it should be a dictionary where the keys are the `sha256`
-        values and the values are dictionaries of metadata.
-        If the sha256 value is not in the metadata dictionary keys, the entire metadata dictionary
-        will be used as metadata for the the sha256 item.
-        You can use this to set the same metadata for all items.
-        
-        Example request body:
-        ```
-        {
-            "sha256": ["<sha256_1>", "<sha256_2>", ...],
-            "metadata": {
-                "<sha256_1>: {
-                    "key1": "value1",
-                    "key2": "value2",
-                    ...
-                },
-                "key1": "value1",
-                "key2": "value2",
-                ...
-            }
-        }
-        ```
-        """,
+Add multiple bookmarks to a namespace.
+The `sha256` values of the items to be bookmarked should be provided in the request body.
+Optionally, metadata can be provided.
+If metadata is provided, it should be a dictionary where the keys are the `sha256`
+values and the values are dictionaries of metadata.
+If the sha256 value is not in the metadata dictionary keys, the entire metadata dictionary
+will be used as metadata for the the sha256 item.
+You can use this to set the same metadata for all items.
+
+Example request body:
+```
+{
+    "sha256": ["<sha256_1>", "<sha256_2>", ...],
+    "metadata": {
+        "<sha256_1>: {
+            "key1": "value1",
+            "key2": "value2",
+            ...
+        },
+        "key1": "value1",
+        "key2": "value2",
+        ...
+    }
+}
+```
+    """,
+    response_model=MessageResult,
 )
 def add_bookmarks_by_sha256(
     namespace: str,
@@ -151,6 +177,7 @@ def add_bookmarks_by_sha256(
     user: str = Query("user"),
     conn: sqlite3.Connection = Depends(get_db_user_data_wl),
 ):
+    c = 0
     for s in items.sha256:
         if items.metadata:
             metadata = items.metadata
@@ -159,11 +186,14 @@ def add_bookmarks_by_sha256(
         else:
             metadata = None
         add_bookmark(conn, s, namespace=namespace, user=user, metadata=metadata)
-    return {"message": "Added bookmarks"}
+        c += 1
+    return MessageResult(message=f"Added {c} bookmarks")
 
 
 @router.delete(
-    "/{namespace}/{sha256}", summary="Delete a bookmark by namespace and sha256"
+    "/{namespace}/{sha256}",
+    summary="Delete a bookmark by namespace and sha256",
+    response_model=MessageResult,
 )
 def delete_bookmark_by_sha256(
     namespace: str,
@@ -172,17 +202,18 @@ def delete_bookmark_by_sha256(
     conn: sqlite3.Connection = Depends(get_db_user_data_wl),
 ):
     remove_bookmark(conn, sha256, namespace=namespace, user=user)
-    return {"message": "Deleted bookmark"}
+    return MessageResult(message="Deleted bookmark")
 
 
 @router.put(
     "/{namespace}/{sha256}",
     summary="Add a bookmark by namespace and sha256",
     description="""
-    Add a bookmark by namespace and sha256.
-    Optionally, metadata can be provided as the request body.
-    Metadata should be a dictionary of key-value pairs.
-    """,
+Add a bookmark by namespace and sha256.
+Optionally, metadata can be provided as the request body.
+Metadata should be a dictionary of key-value pairs.
+""",
+    response_model=MessageResult,
 )
 def add_bookmark_by_sha256(
     namespace: str,
@@ -194,16 +225,23 @@ def add_bookmark_by_sha256(
     add_bookmark(
         conn, sha256, namespace=namespace, user=user, metadata=metadata
     )
-    return {"message": "Added bookmark"}
+    return MessageResult(message="Added bookmark")
+
+
+@dataclass
+class BookmarkMetadata:
+    exists: bool
+    metadata: Optional[Dict] = None
 
 
 @router.get(
     "/{namespace}/{sha256}",
     summary="Get a bookmark by namespace and sha256",
     description="""
-            Get a bookmark by namespace and sha256.
-            Returns whether the bookmark exists and the metadata.
-            """,
+Get a bookmark by namespace and sha256.
+Returns whether the bookmark exists and the metadata.
+    """,
+    response_model=BookmarkMetadata,
 )
 def get_bookmark(
     namespace: str,
@@ -214,4 +252,4 @@ def get_bookmark(
     exists, metadata = get_bookmark_metadata(
         conn, sha256, namespace=namespace, user=user
     )
-    return {"exists": exists, "metadata": metadata}
+    return BookmarkMetadata(exists=exists, metadata=metadata)
