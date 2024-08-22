e56c73a4a735ad9124859912be07fcebff3ec429
reasv <7143787+reasv@users.noreply.github.com>
2024-08-22 23:09:12 +0200

    Build and start next js client

--- src/searchui/router.py
+++ src/searchui/router.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import subprocess
+import threading
 from typing import Tuple
 from venv import logger
 
@@ -7,6 +9,7 @@ from fastapi import APIRouter
 from fastapi_proxy_lib.core.http import ReverseHttpProxy
 from fastapi_proxy_lib.core.websocket import ReverseWebSocketProxy
 from fastapi_proxy_lib.fastapi.router import RouterHelper
+from nodejs_wheel import npm, npx
 
 logger = logging.getLogger(__name__)
 
@@ -14,11 +17,14 @@ logger = logging.getLogger(__name__)
 def get_routers(
     parent_hostname: str, parent_port: int
 ) -> Tuple[APIRouter, APIRouter]:
-    # Node.js client host and port (with fallbacks)
-    client_hostname = os.getenv("CLIENT_HOST", parent_hostname)
-    client_port = int(os.getenv("CLIENT_PORT", 6339))
+    if url := os.getenv("CLIENT_URL"):
+        client_url = url
+    else:
+        client_hostname = os.getenv("CLIENT_HOST", parent_hostname)
+        client_port = int(os.getenv("CLIENT_PORT", 6339))
+        client_url = f"http://{client_hostname}:{client_port}/"
+        run_node_client(client_hostname, client_port)
 
-    client_url = f"http://{client_hostname}:{client_port}/"
     logger.info(f"Client URL: {client_url}")
     reverse_http_proxy = ReverseHttpProxy(base_url=client_url)
     reverse_ws_proxy = ReverseWebSocketProxy(base_url=client_url)
@@ -35,3 +41,31 @@ def get_routers(
     )
 
     return reverse_http_router, reverse_ws_router
+
+
+def run_node_client(hostname: str, port: int):
+    logger.info("Running Node.js client")
+    # The client is in the folder this file is in, under "panoptikon-ui"
+    client_dir = os.path.join(os.path.dirname(__file__), "panoptikon-ui")
+    logger.debug(f"Client directory: {client_dir}")
+    # Save current working directory
+    cwd = os.getcwd()
+    # Change cwd to the client directory
+    os.chdir(client_dir)
+    # Install dependencies
+    npm("install", stdout=subprocess.DEVNULL)
+    # Start the client
+    npx(["--yes", "next", "build"], stdout=subprocess.DEVNULL)
+
+    # Function to start the server in a separate thread
+    def start_server():
+        npx(
+            ["--yes", "next", "start", "-p", str(port), "-H", hostname],
+            stdout=subprocess.DEVNULL,
+        )
+
+    # Start the server in a new thread
+    server_thread = threading.Thread(target=start_server)
+    server_thread.start()
+
+    os.chdir(cwd)
