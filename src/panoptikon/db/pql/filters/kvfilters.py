650ec8a82c9ab0ce7b85132d4a473a8aaff52254
reasv <7143787+reasv@users.noreply.github.com>
2024-09-23 21:00:48 +0200

    multi kv filter

--- src/panoptikon/db/pql/filters/kvfilters.py
+++ src/panoptikon/db/pql/filters/kvfilters.py
@@ -93,6 +93,22 @@ class ArgValuesScalar(ArgValuesBase):
     source_id: Optional[int] = None
 
 
+operatorType = Literal[
+    "eq",
+    "neq",
+    "startswith",
+    "not_startswith",
+    "gt",
+    "gte",
+    "lt",
+    "lte",
+    "endswith",
+    "not_endswith",
+    "contains",
+    "not_contains",
+]
+
+
 class KVFilter(Filter):
     def kv_get_validated(self, args: ArgValuesBase):
         if len(args.get_set_values()) == 0:
@@ -100,23 +116,9 @@ class KVFilter(Filter):
 
         return self.set_validated(True)
 
-    def build_kv_query(
+    def build_multi_kv_query(
         self,
-        operator: Literal[
-            "eq",
-            "neq",
-            "startswith",
-            "not_startswith",
-            "gt",
-            "gte",
-            "lt",
-            "lte",
-            "endswith",
-            "not_endswith",
-            "contains",
-            "not_contains",
-        ],
-        args: ArgValuesBase,
+        queries: List[Tuple[operatorType, ArgValuesBase]],
         context: CTE,
         state: QueryState,
     ) -> CTE:
@@ -130,78 +132,88 @@ class KVFilter(Filter):
         )
 
         criteria = []
-        key_list = []
-        for key, value in args.get_set_values():
-            key_list.append(key)
-            if not isinstance(value, list):
-                if operator == "eq":
-                    criteria.append(get_column(key) == value)
-                elif operator == "neq":
-                    criteria.append(get_column(key) != value)
-                elif operator == "startswith":
-                    criteria.append(get_column(key).startswith(value))
-                elif operator == "not_startswith":
-                    criteria.append(not_(get_column(key).startswith(value)))
-                elif operator == "not_endswith":
-                    criteria.append(not_(get_column(key).endswith(value)))
-                elif operator == "endswith":
-                    criteria.append(get_column(key).endswith(value))
-                elif operator == "contains":
-                    criteria.append(get_column(key).contains(value))
-                elif operator == "not_contains":
-                    criteria.append(not_(get_column(key).contains(value)))
-                elif operator == "gt":
-                    criteria.append(get_column(key) > value)
-                elif operator == "gte":
-                    criteria.append(get_column(key) >= value)
-                elif operator == "lt":
-                    criteria.append(get_column(key) < value)
-                elif operator == "lte":
-                    criteria.append(get_column(key) <= value)
-            else:
-                if operator == "eq":
-                    criteria.append(get_column(key).in_(value))
-                elif operator == "neq":
-                    criteria.append(get_column(key).notin_(value))
-                elif operator == "startswith":
-                    criteria.append(
-                        or_(*[get_column(key).startswith(v) for v in value])
-                    )
-                elif operator == "not_startswith":
-                    criteria.append(
-                        and_(
-                            *[
-                                not_(get_column(key).startswith(v))
-                                for v in value
-                            ]
-                        )
-                    )
-                elif operator == "endswith":
-                    criteria.append(
-                        or_(*[get_column(key).endswith(v) for v in value])
-                    )
-                elif operator == "not_endswith":
-                    criteria.append(
-                        and_(
-                            *[not_(get_column(key).endswith(v)) for v in value]
-                        )
-                    )
-                elif operator == "contains":
-                    criteria.append(
-                        or_(*[get_column(key).contains(v) for v in value])
-                    )
-                elif operator == "not_contains":
-                    criteria.append(
-                        and_(
-                            *[not_(get_column(key).contains(v)) for v in value]
-                        )
-                    )
+        for operator, args in queries:
+            key_list = []
+            for key, value in args.get_set_values():
+                key_list.append(key)
+                if not isinstance(value, list):
+                    if operator == "eq":
+                        criteria.append(get_column(key) == value)
+                    elif operator == "neq":
+                        criteria.append(get_column(key) != value)
+                    elif operator == "startswith":
+                        criteria.append(get_column(key).startswith(value))
+                    elif operator == "not_startswith":
+                        criteria.append(not_(get_column(key).startswith(value)))
+                    elif operator == "not_endswith":
+                        criteria.append(not_(get_column(key).endswith(value)))
+                    elif operator == "endswith":
+                        criteria.append(get_column(key).endswith(value))
+                    elif operator == "contains":
+                        criteria.append(get_column(key).contains(value))
+                    elif operator == "not_contains":
+                        criteria.append(not_(get_column(key).contains(value)))
+                    elif operator == "gt":
+                        criteria.append(get_column(key) > value)
+                    elif operator == "gte":
+                        criteria.append(get_column(key) >= value)
+                    elif operator == "lt":
+                        criteria.append(get_column(key) < value)
+                    elif operator == "lte":
+                        criteria.append(get_column(key) <= value)
                 else:
-                    raise ValueError("Invalid operator for list values")
+                    if operator == "eq":
+                        criteria.append(get_column(key).in_(value))
+                    elif operator == "neq":
+                        criteria.append(get_column(key).notin_(value))
+                    elif operator == "startswith":
+                        criteria.append(
+                            or_(*[get_column(key).startswith(v) for v in value])
+                        )
+                    elif operator == "not_startswith":
+                        criteria.append(
+                            and_(
+                                *[
+                                    not_(get_column(key).startswith(v))
+                                    for v in value
+                                ]
+                            )
+                        )
+                    elif operator == "endswith":
+                        criteria.append(
+                            or_(*[get_column(key).endswith(v) for v in value])
+                        )
+                    elif operator == "not_endswith":
+                        criteria.append(
+                            and_(
+                                *[
+                                    not_(get_column(key).endswith(v))
+                                    for v in value
+                                ]
+                            )
+                        )
+                    elif operator == "contains":
+                        criteria.append(
+                            or_(*[get_column(key).contains(v) for v in value])
+                        )
+                    elif operator == "not_contains":
+                        criteria.append(
+                            and_(
+                                *[
+                                    not_(get_column(key).contains(v))
+                                    for v in value
+                                ]
+                            )
+                        )
+                    else:
+                        raise ValueError("Invalid operator for list values")
+            if not state.item_data_query:
+                if contains_text_columns(key_list):
+                    raise ValueError(
+                        "Text columns are not allowed in this context"
+                    )
 
         if not state.item_data_query:
-            if contains_text_columns(key_list):
-                raise ValueError("Text columns are not allowed in this context")
             return self.wrap_query(
                 select(*get_std_cols(context, state))
                 .join(
@@ -251,7 +263,7 @@ class Equals(KVFilter):
         return self.kv_get_validated(self.eq)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query("eq", self.eq, context, state)
+        return self.build_multi_kv_query([("eq", self.eq)], context, state)
 
 
 class NotEquals(KVFilter):
@@ -261,7 +273,7 @@ class NotEquals(KVFilter):
         return self.kv_get_validated(self.neq)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query("neq", self.neq, context, state)
+        return self.build_multi_kv_query([("neq", self.neq)], context, state)
 
 
 class In(KVFilter):
@@ -271,7 +283,7 @@ class In(KVFilter):
         return self.kv_get_validated(self.in_)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query("eq", self.in_, context, state)
+        return self.build_multi_kv_query([("eq", self.in_)], context, state)
 
 
 class NotIn(KVFilter):
@@ -281,7 +293,7 @@ class NotIn(KVFilter):
         return self.kv_get_validated(self.nin)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query("neq", self.nin, context, state)
+        return self.build_multi_kv_query([("neq", self.nin)], context, state)
 
 
 class GtThan(KVFilter):
@@ -291,7 +303,7 @@ class GtThan(KVFilter):
         return self.kv_get_validated(self.gt)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query("gt", self.gt, context, state)
+        return self.build_multi_kv_query([("gt", self.gt)], context, state)
 
 
 class GtThanOrEq(KVFilter):
@@ -301,7 +313,7 @@ class GtThanOrEq(KVFilter):
         return self.kv_get_validated(self.gte)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query("gte", self.gte, context, state)
+        return self.build_multi_kv_query([("gte", self.gte)], context, state)
 
 
 class LessThan(KVFilter):
@@ -311,7 +323,7 @@ class LessThan(KVFilter):
         return self.kv_get_validated(self.lt)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query("lt", self.lt, context, state)
+        return self.build_multi_kv_query([("lt", self.lt)], context, state)
 
 
 class LessThanOrEq(KVFilter):
@@ -321,7 +333,7 @@ class LessThanOrEq(KVFilter):
         return self.kv_get_validated(self.lte)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query("lte", self.lte, context, state)
+        return self.build_multi_kv_query([("lte", self.lte)], context, state)
 
 
 class StartsWith(KVFilter):
@@ -331,8 +343,8 @@ class StartsWith(KVFilter):
         return self.kv_get_validated(self.startswith)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query(
-            "startswith", self.startswith, context, state
+        return self.build_multi_kv_query(
+            [("startswith", self.startswith)], context, state
         )
 
 
@@ -343,8 +355,8 @@ class NotStartsWith(KVFilter):
         return self.kv_get_validated(self.not_startswith)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query(
-            "not_startswith", self.not_startswith, context, state
+        return self.build_multi_kv_query(
+            [("not_startswith", self.not_startswith)], context, state
         )
 
 
@@ -355,7 +367,9 @@ class EndsWith(KVFilter):
         return self.kv_get_validated(self.endswith)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query("endswith", self.endswith, context, state)
+        return self.build_multi_kv_query(
+            [("endswith", self.endswith)], context, state
+        )
 
 
 class NotEndsWith(KVFilter):
@@ -365,8 +379,8 @@ class NotEndsWith(KVFilter):
         return self.kv_get_validated(self.not_endswith)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query(
-            "not_endswith", self.not_endswith, context, state
+        return self.build_multi_kv_query(
+            [("not_endswith", self.not_endswith)], context, state
         )
 
 
@@ -377,7 +391,9 @@ class Contains(KVFilter):
         return self.kv_get_validated(self.contains)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query("contains", self.contains, context, state)
+        return self.build_multi_kv_query(
+            [("contains", self.contains)], context, state
+        )
 
 
 class NotContains(KVFilter):
@@ -387,9 +403,48 @@ class NotContains(KVFilter):
         return self.kv_get_validated(self.not_contains)
 
     def build_query(self, context: CTE, state: QueryState) -> CTE:
-        return self.build_kv_query(
-            "not_contains", self.not_contains, context, state
+        return self.build_multi_kv_query(
+            [("not_contains", self.not_contains)], context, state
         )
+
+
+class MatchOps(BaseModel):
+    eq: Optional[ArgValuesScalar] = None
+    neq: Optional[ArgValuesScalar] = None
+    in_: Optional[ArgValues] = None
+    nin: Optional[ArgValues] = None
+    gt: Optional[ArgValuesScalar] = None
+    gte: Optional[ArgValuesScalar] = None
+    lt: Optional[ArgValuesScalar] = None
+    lte: Optional[ArgValuesScalar] = None
+    startswith: Optional[ArgValues] = None
+    not_startswith: Optional[ArgValues] = None
+    endswith: Optional[ArgValues] = None
+    not_endswith: Optional[ArgValues] = None
+    contains: Optional[ArgValues] = None
+    not_contains: Optional[ArgValues] = None
+
+
+class Values(KVFilter):
+    values: MatchOps
+
+    def _validate(self):
+        for operator, _ in self.values.model_dump().items():
+            args = getattr(self.values, operator, None)
+            if args is not None:
+                assert isinstance(args, ArgValuesBase), f"Invalid args: {args}"
+                if len(args.get_set_values()) == 0:
+                    setattr(self.values, operator, None)
+                    continue
+                # Find at least one valid operator
+                return self.set_validated(True)
+
+    def build_query(self, context: CTE, state: QueryState) -> CTE:
+        queries = []
+        for operator, args in self.values.model_dump().items():
+            if args is not None:
+                queries.append((operator, args))
+        return self.build_multi_kv_query(queries, context, state)
 
 
 ValueFilters = Union[
