37554dce06bd414142034373b52f0ce6a8091a31
reasv <7143787+reasv@users.noreply.github.com>
2024-09-24 22:06:43 +0200

    Add text filter to semantic image search

--- src/panoptikon/db/pql/filters/sortable/image_embeddings.py
+++ src/panoptikon/db/pql/filters/sortable/image_embeddings.py
@@ -6,10 +6,11 @@ import numpy as np
 import PIL
 import PIL.Image
 from pydantic import BaseModel, Field, PrivateAttr
-from sqlalchemy import and_, func, literal, not_
+from sqlalchemy import and_, func, literal, not_, or_
 from sqlalchemy.sql.expression import CTE, select
 
 from inferio.impl.utils import deserialize_array
+from panoptikon.db.pql.filters.sortable.item_similarity import SourceArgs
 from panoptikon.db.pql.filters.sortable.sortable_filter import SortableFilter
 from panoptikon.db.pql.filters.sortable.text_embeddings import (
     EmbedArgs,
@@ -74,6 +75,14 @@ They are generated *separately* from the image embeddings, using a different job
 Run a batch job with the same clip model for both image and text embeddings to use this setting.
         """,
     )
+    src_text: Optional[SourceArgs] = Field(
+        default=None,
+        description="""
+Filters and options to apply on source text.
+Can exclusively be used with `clip_xmodal` set to True.
+Otherwise, it will be ignored, as it only applies to text embeddings.
+""",
+    )
 
 
 class SemanticImageSearch(SortableFilter):
@@ -101,6 +110,11 @@ Search for image using semantic search on image embeddings.
             self.image_embeddings._embedding = extract_embeddings(
                 self.image_embeddings.query
             )
+        if (
+            not self.image_embeddings.clip_xmodal
+            and self.image_embeddings.src_text is not None
+        ):
+            self.image_embeddings.src_text = None
 
         return self.set_validated(True)
 
@@ -108,6 +122,7 @@ Search for image using semantic search on image embeddings.
         self.raise_if_not_validated()
         from panoptikon.db.pql.tables import (
             embeddings,
+            extracted_text,
             item_data,
             items,
             setters,
@@ -139,6 +154,74 @@ Search for image using semantic search on image embeddings.
                 item_data.c.id == embeddings.c.id,
             )
         )
+
+        src_setters = setters.alias("src_setters")
+        src_item_data = item_data.alias("src_item_data")
+
+        if args.src_text:
+            # Filter text embeddings based on source text
+            src_args = args.src_text
+            # Join with extracted_text and apply filters
+            # If the query is cross-modal, we only apply the source text filters to the text embeddings
+            embeddings_query = embeddings_query.join(
+                src_item_data,
+                src_item_data.c.id == item_data.c.source_id,
+                isouter=True,
+            )
+            if src_args.setter_names:
+                embeddings_query = embeddings_query.join(
+                    src_setters,
+                    src_setters.c.id == src_item_data.c.setter_id,
+                    isouter=True,
+                )
+            join_text = False
+            conditions = []
+            if src_args.setter_names:
+                conditions.append(src_setters.c.name.in_(src_args.setter_names))
+
+            if src_args.languages:
+                join_text = True
+                conditions.append(
+                    extracted_text.c.language.in_(src_args.languages)
+                )
+
+            if src_args.min_confidence > 0:
+                join_text = True
+                conditions.append(
+                    extracted_text.c.confidence >= src_args.min_confidence
+                )
+
+            if src_args.min_language_confidence > 0:
+                join_text = True
+                conditions.append(
+                    extracted_text.c.language_confidence
+                    >= src_args.min_language_confidence
+                )
+
+            if src_args.min_length > 0:
+                join_text = True
+                conditions.append(
+                    extracted_text.c.text_length >= src_args.min_length
+                )
+            if (
+                args.src_text.confidence_weight != 0
+                or args.src_text.language_confidence_weight != 0
+            ):
+                join_text = True
+
+            if join_text:
+                embeddings_query = embeddings_query.join(
+                    extracted_text,
+                    extracted_text.c.id == item_data.c.source_id,
+                    isouter=True,
+                )
+            # Only apply the source text filters to the text embeddings
+            embeddings_query = embeddings_query.where(
+                or_(
+                    src_item_data.c.id.is_(None),
+                    and_(*conditions),
+                )
+            )
 
         embeddings_query = embeddings_query.join(
             context,
