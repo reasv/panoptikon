64c737d329dd5b75faffbe50a303a9c376a91e8f
reasv <7143787+reasv@users.noreply.github.com>
2024-10-05 23:24:44 +0200

    Run migrations on all databases

--- src/panoptikon/db/migrations/storage/env.py
+++ src/panoptikon/db/migrations/storage/env.py
@@ -3,27 +3,39 @@ import logging
 from alembic import context
 from sqlalchemy import create_engine
 
-from panoptikon.db import get_db_paths
+from panoptikon.db import get_db_lists, get_db_paths
 
 logger = logging.getLogger(__name__)
 
 
 def run_migrations_online():
     """Run migrations in 'online' mode using SQLAlchemy's create_engine."""
-    db_file, user_db_file, storage_db_file = get_db_paths()
-    # index_db_url = f"sqlite:///{db_file}"
-    # user_data_db_url = f"sqlite:///{user_db_file}"
-    storage_db_url = f"sqlite:///{storage_db_file}"
-    # Create SQLAlchemy engines for the databases
-    index_engine = create_engine(storage_db_url)
+    # Get default db first
+    default_db_file, user_db_file, default_storage_db_file = get_db_paths()
+    db_files = [default_storage_db_file]
+    # Get other dbs
+    index_dbs, user_data_dbs = get_db_lists()
+    for index_db in index_dbs:
+        index_db_file, user_db_file, storage_db_file = get_db_paths(
+            index_db=index_db
+        )
+        if storage_db_file not in db_files:
+            db_files.append(storage_db_file)
 
-    # Get a connection from the main database engine
-    with index_engine.connect() as connection:
-        # Set up Alembic's migration context
-        context.configure(connection=connection, target_metadata=None)
+    for storage_db_file in db_files:
+        # index_db_url = f"sqlite:///{db_file}"
+        # user_data_db_url = f"sqlite:///{user_db_file}"
+        storage_db_url = f"sqlite:///{storage_db_file}"
+        # Create SQLAlchemy engines for the databases
+        index_engine = create_engine(storage_db_url)
 
-        # Run the migrations within a transaction
-        context.run_migrations()
+        # Get a connection from the main database engine
+        with index_engine.connect() as connection:
+            # Set up Alembic's migration context
+            context.configure(connection=connection, target_metadata=None)
+
+            # Run the migrations within a transaction
+            context.run_migrations()
 
 
 # Alembic expects a function to call migrations either in offline or online mode.
--- src/panoptikon/db/migrations/user_data/env.py
+++ src/panoptikon/db/migrations/user_data/env.py
@@ -3,7 +3,7 @@ import logging
 from alembic import context
 from sqlalchemy import create_engine
 
-from panoptikon.db import get_db_paths
+from panoptikon.db import get_db_lists, get_db_paths
 
 logger = logging.getLogger(__name__)
 
@@ -11,20 +11,30 @@ logger = logging.getLogger(__name__)
 def run_migrations_online():
     """Run migrations in 'online' mode using SQLAlchemy's create_engine."""
 
-    db_file, user_db_file, storage_db_file = get_db_paths()
-    # index_db_url = f"sqlite:///{db_file}"
-    user_data_db_url = f"sqlite:///{user_db_file}"
-    # storage_db_url = f"sqlite:///{storage_db_file}"
-    # Create SQLAlchemy engines for the databases
-    index_engine = create_engine(user_data_db_url)
+    db_file, default_user_db_file, storage_db_file = get_db_paths()
+    db_files = [default_user_db_file]
+    index_dbs, user_data_dbs = get_db_lists()
+    for user_db in user_data_dbs:
+        db_file, user_db_file, storage_db_file = get_db_paths(
+            user_data_db=user_db
+        )
+        if user_db_file not in db_files:
+            db_files.append(user_db_file)
 
-    # Get a connection from the main database engine
-    with index_engine.connect() as connection:
-        # Set up Alembic's migration context
-        context.configure(connection=connection, target_metadata=None)
+    for user_db_file in db_files:
+        # index_db_url = f"sqlite:///{db_file}"
+        user_data_db_url = f"sqlite:///{user_db_file}"
+        # storage_db_url = f"sqlite:///{storage_db_file}"
+        # Create SQLAlchemy engines for the databases
+        index_engine = create_engine(user_data_db_url)
 
-        # Run the migrations within a transaction
-        context.run_migrations()
+        # Get a connection from the main database engine
+        with index_engine.connect() as connection:
+            # Set up Alembic's migration context
+            context.configure(connection=connection, target_metadata=None)
+
+            # Run the migrations within a transaction
+            context.run_migrations()
 
 
 # Alembic expects a function to call migrations either in offline or online mode.
