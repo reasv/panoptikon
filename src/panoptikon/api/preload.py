715bf29301e342d642c65029a32fab70804ce124
reasv <7143787+reasv@users.noreply.github.com>
2024-10-20 03:41:24 +0200

    Explicitly unload when preload is disabled

--- src/panoptikon/api/preload.py
+++ src/panoptikon/api/preload.py
@@ -3,21 +3,30 @@ from datetime import datetime, timedelta
 
 from panoptikon.api.routers.utils import get_db_readonly
 from panoptikon.config import retrieve_system_config
+from panoptikon.data_extractors.models import get_inference_api_client
 from panoptikon.db import get_database_connection
 from panoptikon.db.extraction_log import get_existing_setters
 
 logger = logging.getLogger(__name__)
 
 # Store the next renewal time for each model in a dictionary
-_next_renewal_times = {}
+next_renewal_times = {}
+
+preload_on = False
 
 
-def preload_embedding_models(index_db: str, ttl: int = 600):
-    global _next_renewal_times
-
+def preload_embedding_models(index_db: str, ttl: int = 3600):
+    global next_renewal_times, preload_on
     system_config = retrieve_system_config(index_db)
     if not system_config.preload_embedding_models:
+        if preload_on:
+            logger.info("Disabling model preloading, clearing cache...")
+            next_renewal_times = {}
+            preload_on = False
+            client = get_inference_api_client()
+            client.clear_cache("preload")
         return
+    preload_on = True
     conn = get_database_connection(**get_db_readonly(index_db, None))
     try:
         from panoptikon.data_extractors.models import ModelOptsFactory
@@ -35,14 +44,13 @@ def preload_embedding_models(index_db: str, ttl: int = 600):
 
         current_time = datetime.now()
         for setter in embedding_setters:
-            next_renewal_time = _next_renewal_times.get(setter)
+            next_renewal_time = next_renewal_times.get(setter)
             if not next_renewal_time or current_time >= next_renewal_time:
                 model = ModelOptsFactory.get_model(setter)
                 model.load_model("preload", len(embedding_setters), ttl)
-                _next_renewal_times[setter] = current_time + timedelta(
+                next_renewal_times[setter] = current_time + timedelta(
                     seconds=ttl
                 )
-
     except Exception as e:
         logger.error(f"Failed to preload embedding models: {e}", exc_info=True)
     finally:
