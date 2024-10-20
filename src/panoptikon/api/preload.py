0d82792b611e8b0b18fdadb1c61427a42266dbcd
reasv <7143787+reasv@users.noreply.github.com>
2024-10-20 03:36:46 +0200

    Avoid redundant preloads

--- src/panoptikon/api/preload.py
+++ src/panoptikon/api/preload.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime, timedelta
 
 from panoptikon.api.routers.utils import get_db_readonly
 from panoptikon.config import retrieve_system_config
@@ -7,8 +8,13 @@ from panoptikon.db.extraction_log import get_existing_setters
 
 logger = logging.getLogger(__name__)
 
+# Store the next renewal time for each model in a dictionary
+_next_renewal_times = {}
 
-def preload_embedding_models(index_db: str):
+
+def preload_embedding_models(index_db: str, ttl: int = 600):
+    global _next_renewal_times
+
     system_config = retrieve_system_config(index_db)
     if not system_config.preload_embedding_models:
         return
@@ -26,9 +32,17 @@ def preload_embedding_models(index_db: str):
         ]
         if not embedding_setters:
             return
+
+        current_time = datetime.now()
         for setter in embedding_setters:
-            model = ModelOptsFactory.get_model(setter)
-            model.load_model("preload", len(embedding_setters), 180)
+            next_renewal_time = _next_renewal_times.get(setter)
+            if not next_renewal_time or current_time >= next_renewal_time:
+                model = ModelOptsFactory.get_model(setter)
+                model.load_model("preload", len(embedding_setters), ttl)
+                _next_renewal_times[setter] = current_time + timedelta(
+                    seconds=ttl
+                )
+
     except Exception as e:
         logger.error(f"Failed to preload embedding models: {e}", exc_info=True)
     finally:
