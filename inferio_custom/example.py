ee20137dcd9a69c75ec1976c5e9597470badd385
reasv <7143787+reasv@users.noreply.github.com>
2025-06-30 00:19:45 +0200

    Improve model example

--- inferio_custom/example.py
+++ inferio_custom/example.py
@@ -9,6 +9,15 @@ class ExampleModel(InferenceModel):
     Example implementation of a custom InferenceModel.
     You must implement the abstract methods: name, load, predict, unload.
     """
+    def __init__(
+        self,
+        **kwargs  # Use kwargs to accept any additional parameters
+    ):
+        # __init__ receives the model's configuration object from the toml config through kwargs.
+        # You can initialize any resources or configurations here
+        # But do not load model weights into memory or perform other heavy tasks here.
+        pass
+
     @classmethod
     def name(cls) -> str:
         # Return a unique name for this model implementation
@@ -17,6 +26,8 @@ class ExampleModel(InferenceModel):
 
     def load(self) -> None:
         # Load model weights/resources here
+        # Downloading of missing weights or resources should be done here.
+        # This method is called once when the model is loaded into memory.
         pass
 
     def predict(self, inputs: Sequence[PredictionInput]) -> Sequence[str]:
@@ -28,6 +39,8 @@ class ExampleModel(InferenceModel):
 
     def unload(self) -> None:
         # Cleanup resources if needed
+        # Generally, this method can be left empty because each model
+        # is loaded into its own process and the process will be terminated on unload.
         pass
 
 # This variable must be present for your custom model to be correctly registered
