61639ed93dc57a145c89e5cd7d256071c7988740
reasv <7143787+reasv@users.noreply.github.com>
2026-01-14 18:15:03 +0100

    Initialize rust crate for panoptikon-gateway

---
+++ gateway/src/main.rs
@@ -0,0 +1,23 @@
+use axum::{Router, routing::get};
+use tracing_subscriber::EnvFilter;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env().add_directive("info".parse().unwrap()))
+        .init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::info!("gateway listening on 0.0.0.0:3000");
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
+}
+async fn root() -> &'static str {
+    "Hello, World!"
+}
